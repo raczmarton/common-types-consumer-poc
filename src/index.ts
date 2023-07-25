722ff306d83e81a0dc15f2ce38e73175e9405b4c
marton.racz <martonracz@apexlab.io>
2023-07-25 08:34:22 +0200

    consumer stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import { initTRPC } from '@trpc/server';
+import { z } from 'zod';
+import { User } from '@raczmarton/common-types-poc'
+
+const trpc = initTRPC.create();
+trpc.router({
+    getUser: trpc.procedure.input(z.number()).query((input) => {
+        return {} as User;
+    })
+});
